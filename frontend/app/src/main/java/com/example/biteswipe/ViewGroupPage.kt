da5ed49a574e7a51ded1ebad62b92a1e6802ae0e
Varun Seshadri <vsesh@student.ubc.ca>
2025-03-02 20:21:28 -0800

    ViewGroupPage fully functional

--- frontend/app/src/main/java/com/example/biteswipe/ViewGroupPage.kt
+++ frontend/app/src/main/java/com/example/biteswipe/ViewGroupPage.kt
@@ -1,6 +1,8 @@
 package com.example.biteswipe
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.Toast
@@ -13,7 +15,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.biteswipe.adapter.UserAdapterNoKick
 import com.example.biteswipe.cards.UserCard
 import com.example.biteswipe.jsonFormats.sessionDetails
-import org.json.JSONObject
 
 class ViewGroupPage : AppCompatActivity(), ApiHelper {
     private lateinit var users: MutableList<UserCard>
@@ -23,6 +24,75 @@ class ViewGroupPage : AppCompatActivity(), ApiHelper {
     private lateinit var userId: String
     private lateinit var session: sessionDetails
     private var sessionInitialized = false
+    private val handler = Handler(Looper.getMainLooper())
+    private val updateUsers = object: Runnable {
+        override fun run() {
+//            TODO: In this function, implement the logic that takes us to start matching.
+//            Ensure that handler dies after starting new activity
+
+            val endpoint = "/sessions/$sessionId"
+            apiRequest(
+                context = this@ViewGroupPage,
+                endpoint = endpoint,
+                method = "GET",
+                onSuccess = { response ->
+                    session = parseSessionData(response)
+                    Log.d(TAG, "Session Details: $session")
+                    users.clear()
+                    for (participant in session.participants) {
+                        Log.d(TAG, "Participant: $participant")
+                        val epoint = "/users/${participant.userId._id}"
+                        apiRequest(
+                            context = this@ViewGroupPage,
+                            endpoint = epoint,
+                            method = "GET",
+                            onSuccess = { response ->
+                                Log.d(TAG, "User Details: ${response.getString("displayName")}")
+                                val userName = response.getString("displayName")
+//                                val profilePicResId = R.drawable.ic_settings // Assuming you have a default image here
+                                val userId = participant.userId._id
+                                // Add the UserCard to the list
+                                users.add(UserCard(userName, R.drawable.ic_group, userId))
+                                adapter.notifyDataSetChanged()
+                            },
+                            onError = { code, message ->
+                                Log.d(TAG, "Error fetching user details: $message")
+                                Toast.makeText(
+                                    this@ViewGroupPage,
+                                    "Could not fetch user details",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                val userName = "Loading..."
+//                                val profilePicResId = R.drawable.ic_settings
+                                val userId = participant.userId._id
+                                // Add the UserCard to the list
+                                users.add(UserCard(userName, R.drawable.ic_settings, userId))
+                                adapter.notifyDataSetChanged()
+                            }
+                        )
+//                        TODO: Profile Pics
+                    }
+                },
+                onError = { code, message ->
+                    Log.d(TAG, "Error fetching users: $message")
+                    Toast.makeText(
+                        this@ViewGroupPage,
+                        "Could not fetch users",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    users = mutableListOf(
+                        UserCard("John Doe", R.drawable.ic_settings, "1234567890"),
+                        UserCard("Jane Doe", R.drawable.ic_settings, "0987654321"),
+                        UserCard("Mike Tyson", R.drawable.ic_launcher_background, "1111111111")
+                    )
+                    adapter.notifyDataSetChanged()
+                }
+            )
+            handler.postDelayed(this, 5000)
+        }
+    }
+    private var TAG = "ViewGroupPage"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +103,7 @@ class ViewGroupPage : AppCompatActivity(), ApiHelper {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         sessionId = intent.getStringExtra("sessionId") ?: ""
         if(sessionId == ""){
             Toast.makeText(this, "Error: SessionID Invalid", Toast.LENGTH_SHORT).show()
@@ -45,51 +116,15 @@ class ViewGroupPage : AppCompatActivity(), ApiHelper {
             finish()
         }
 
-//        TODO: API Call to fetch users from backend (PERSISTENT)
-//        TODO: Make this call repeat over intervals to get a clear list of users (thread?)
-        val endpoint = "/sessions/$sessionId"
-        apiRequest(
-            context = this,
-            endpoint = endpoint,
-            method = "GET",
-            onSuccess = { response ->
-                session  = parseSessionData(response)
-                sessionInitialized = true
-                Log.d("ViewGroupPage", "Session Details: $session")
-            }
-        )
-        if(sessionInitialized) {
-            if (session.participants.isNotEmpty()) {
-                users.clear()
-                for (participant in session.participants) {
-                    val userName =
-                        participant.userId.displayName // Access the displayName of the user
-                    val profilePicResId =
-                        R.drawable.ic_settings // Assuming you have a default image here
 
-                    // Add the UserCard to the list
-                    users.add(UserCard(userName, profilePicResId, participant.userId._id))
-                }
-            } else {
-                users = mutableListOf(
-                    UserCard("John Doe", R.drawable.ic_settings, "1234567890"),
-                    UserCard("Jane Doe", R.drawable.ic_settings, "0987654321"),
-                    UserCard("Mike Tyson", R.drawable.ic_launcher_background, "1111111111")
-                )
-            }
-        } else {
-            users = mutableListOf(
-                UserCard("John Doe", R.drawable.ic_settings, "1234567890"),
-                UserCard("Jane Doe", R.drawable.ic_settings, "0987654321"),
-                UserCard("Mike Tyson", R.drawable.ic_launcher_background, "1111111111")
-            )
-        }
+        users = mutableListOf()
 //        TODO: Implement Dynamic Render
         recyclerView = findViewById(R.id.view_users_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = UserAdapterNoKick(this, users)
         recyclerView.adapter = adapter
 
+        updateUsers()
 
         val leaveGroupButton = findViewById<ImageButton>(R.id.leave_group_button)
         leaveGroupButton.setOnClickListener {
@@ -113,5 +148,14 @@ class ViewGroupPage : AppCompatActivity(), ApiHelper {
 
     //        TODO: Implement logic to indicate matching started, and open activity
 //        TODO: API Call to start matching
+    }
+
+    private fun updateUsers() {
+        handler.post(updateUsers)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(updateUsers)
     }
 }
