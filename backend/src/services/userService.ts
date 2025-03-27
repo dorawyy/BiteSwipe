657064774fab28d6b31f726f53f4b6357baa2ea3
Lakshya Saroha <112802205+Lakshya013@users.noreply.github.com>
2025-03-27 00:16:54 -0700

    Merge pull request #70 from BiteSwipe321/lakshya/backend_m6
    Lakshya/backend m6
    
    (cherry picked from commit 8642541c3bb811421c920025474cc62db9f7e3a1)

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,5 +1,6 @@
 import { Model, Types } from 'mongoose';
 import { UserModel, UserLean, User } from '../models/user';
+import { sendNotification } from '../config/firebase';
 
 export class UserService {
   private userModel: Model<User>;
@@ -46,7 +47,7 @@ export class UserService {
         return null;
       }
 
-      return user;
+      return user as UserLean;
     } catch (error) {
       console.error('Error fetching user by ID:', error);
       throw new Error('Failed to fetch user by ID');
@@ -80,7 +81,7 @@ export class UserService {
       const updatedUser = await this.userModel
         .findByIdAndUpdate(
           userId,
-          { $push: { fcmTokens: fcmToken } },
+          { $addToSet: { fcmTokens: fcmToken } }, 
           { new: true }
         )
         .lean();
@@ -89,7 +90,7 @@ export class UserService {
         throw new Error('User not found');
       }
 
-      return updatedUser;
+      return updatedUser  as UserLean;
     } catch (error: any) {
       console.error('Error updating FCM token:', error);
       if (error.message === 'User not found') {
@@ -98,4 +99,242 @@ export class UserService {
       throw new Error('Failed to update FCM token');
     }
   }
+
+  async sendFriendRequest(userEmail: string, friendEmail: string): Promise<UserLean | null> {
+    if(userEmail.trim().length === 0 || friendEmail.trim().length === 0) {
+      throw new Error('User email and friend email is required');
+    }
+
+    const userId = await this.getUserByEmail(userEmail);
+
+    if (!userId) {
+      throw new Error('User not found');
+    }
+
+    const friend = await this.getUserByEmail(friendEmail);
+
+    if (!friend) {
+      throw new Error('User added not found');
+    }
+
+    if(userId.friendList.some(f => f.userId.toString() === friend._id.toString())) {
+      throw new Error('Already a friend');
+    }
+    // check if the request already exists
+  
+    try {
+      const updatedUser = await this.userModel
+        .findByIdAndUpdate(
+          friend._id,
+          { $push: { pendingRequest:{userId: userId._id, displayName: userId.displayName, email: userId.email}}},
+          { new: true, unique: true }
+        )
+        .lean();
+
+      if (!updatedUser) {
+        throw new Error('User not found');
+      }
+      
+      // send notification 
+      const fcmToken = friend.fcmTokens;
+      console.log(`FCMTokens for user ${friend.email}: ${fcmToken} `);
+      if(fcmToken){
+        const notificationData = {
+          type: "friend",
+          title: "New Friend Request",
+          message: `You've got a request from ${userId.displayName}`,
+          friendId: friend._id.toString()
+        }
+        if(fcmToken.length == 1){
+          try {
+            sendNotification(fcmToken.toString(), "New Friend Request", `You've got a request from ${userId.displayName}`, notificationData);
+          }
+          catch (error) {
+            console.error("Could not send notification")
+          }
+        } else {
+          for(let token in fcmToken) {
+            try{
+              sendNotification(token, "New Friend Request", `You've got a request from ${userId.displayName}`, notificationData);
+            } catch (error) {
+              console.error("Could not send notification");
+            }
+          }
+        }
+      }
+
+      return updatedUser as UserLean;
+    } catch (error: any) {
+      console.error('Error sending friend request:', error);
+      if (error.message === 'User not found') {
+        throw error;
+      }
+      throw new Error('Failed to send friend request');
+    }
+  }
+
+  async acceptFriendRequest   (userEmail: string, friendEmail: string ) : Promise<string[] | null> {
+    if(userEmail.trim().length === 0 || friendEmail.trim().length === 0) {
+      throw new Error('User email and friend email is required');
+    }    
+
+    const userId = await this.getUserByEmail(userEmail);
+
+    if (!userId) {
+      throw new Error('User not found');
+    }
+
+    const friend = await this.getUserByEmail(friendEmail);
+
+    if (!friend) {
+      throw new Error('Friend not found');
+    }
+    
+  
+    try {
+      // update user's friendList
+      const updatedUser = await this.userModel
+        .findByIdAndUpdate(
+          userId,
+          { $push: { friendList: {userId: friend._id, displayName: friend.displayName, email: friend.email}}, $pull: { pendingRequest: {userId: friend._id }}},
+          { new: true, unique: true }
+        )
+        .lean();
+
+      if (!updatedUser) {
+        throw new Error('User not found');
+      }
+
+      // update friend's friendList
+      const updatedFriend = await this.userModel
+      .findByIdAndUpdate(
+        friend,
+        { $push: {friendList: {userId: userId._id, displayName: userId.displayName, email: userId.email }}},
+        {new: true, unique: true}
+      )
+      .lean();
+
+      if(!updatedFriend) {
+        throw new Error('Friend not found');
+      }
+
+      const friendDisplayName: string[] = [];
+
+      for(const friendId of updatedUser.friendList) {
+        const friend = await this.userModel.findById(friendId).lean();
+        if(friend) {
+          friendDisplayName.push(friend?.displayName);
+        }
+      }
+
+      return friendDisplayName as string[];
+    } catch (error: any) {
+      console.error('Error accepting friend request:', error);
+      if (error.message === 'User not found') {
+        throw error;
+      }
+      throw new Error('Failed to accept friend request');
+    }
+  }
+
+  async rejectFriendRequest(userEmail: string, friendEmail: string): Promise<UserLean | null> {
+    if(userEmail.trim().length === 0 || friendEmail.trim().length === 0) {
+      throw new Error('User email and friend email is required');
+    }
+
+    const userId = await this.getUserByEmail(userEmail);
+
+    if (!userId) {
+      throw new Error('User not found');
+    }
+
+    const friend = await this.getUserByEmail(friendEmail);
+
+    if (!friend) {
+      throw new Error('Friend not found');
+    }
+
+    try {
+      const updatedUser = await this.userModel
+        .findByIdAndUpdate(
+          userId,
+          { $pull: { pendingRequest: {userId: friend._id }}},
+          { new: true, unique: true }
+        )
+        .lean();
+
+      if (!updatedUser) {
+        throw new Error('User not found');
+      }
+
+      return updatedUser as UserLean;
+    } catch (error: any) {
+      console.error('Error rejecting friend request:', error);
+      if (error.message === 'User not found') {
+        throw error;
+      }
+      throw new Error('Failed to reject friend request');
+    }
+  }
+
+  async removeFriend(userEmail: string, friendEmail: string): Promise<string[] | null> {
+    if(userEmail.trim().length === 0 || friendEmail.trim().length === 0) {
+      throw new Error('User email and friend email is required');
+    }
+
+    const userId = await this.getUserByEmail(userEmail);
+
+    if (!userId) {
+      throw new Error('User not found');
+    }
+
+    const friend = await this.getUserByEmail(friendEmail);
+
+    if (!friend) {
+      throw new Error('Friend not found');
+    }
+
+    try {
+      const updatedUser = await this.userModel
+        .findByIdAndUpdate(
+          userId._id,
+          { $pull: { friendList: {userId: friend._id }}},
+          { new: true, unique: true }
+        )
+        .lean();
+
+      if (!updatedUser) {
+        throw new Error('User not found');
+      }
+
+      const updatedFriend = await this.userModel
+        .findByIdAndUpdate(
+          friend._id,
+          { $pull: {friendList: {userId: userId._id}}},
+          { new: true, unique: true}
+        )
+        .lean();
+      if(!updatedFriend) {
+        throw new Error('Not removed from friend');
+      }
+
+      const friendDisplayName: string[] = [];
+
+      for(const friendId of updatedUser.friendList) {
+        const friend = await this.userModel.findById(friendId).lean();
+        if(friend) {
+          friendDisplayName.push(friend?.displayName);
+        }
+      }
+
+      return friendDisplayName as string[];
+    } catch (error: any) {
+      console.error('Error removing friend:', error);
+      if (error.message === 'User not found') {
+        throw error;
+      }
+      throw new Error('Failed to remove friend');
+    }
+  }
+
 }
